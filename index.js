a1363c684e26f0b2ccb72a8f9d0d7a182631ac72
Matej Lednicky <matej@lednicky.name>
2022-10-21 12:58:03 +0200

    add radio rock (new antena) crawler

--- index.js
+++ index.js
@@ -3,23 +3,37 @@ const btoa = require("btoa");
 
 const sleep = (ms) => new Promise(res => setTimeout(res, ms))
 
-const getTracks = async (url) => {
-	let data = []
+const extractors = {
+	map886: (result) => {
+		const data = result && result.data || []
+		return data.map(track => {
+			const { name, title } = track || {}
+			if (name && title) {
+				return `${name} - ${title}`
+			}
+		})
+	},
+	mapRadioRock: (result) => {
+		const data = result || []
+		return data.reverse().map(track => {
+			const { artist, title } = track || {}
+			if (artist && title) {
+				return `${artist} - ${title}`
+			}
+		})
+	}
+}
 
+const getTracks = async (url, extractor) => {
+	let result = {}
 	try {
 		const response = await fetch(url);
-		const result = await response.json();
-		data = result && result.data || []
+		result = await response.json();
 	} catch(e) {
 		console.error('Failed while fetching tracks:'. e)
 	}
 
-	return data.map(track => {
-		const { name, title } = track || {}
-		if (name && title) {
-			return `${name} - ${title}`
-		}
-	}).filter(v => !!v);
+	return extractor(result).filter(v => !!v);
 }
 
 const register = async () => {
@@ -62,7 +76,7 @@ const auth = async (code, useAuthCode = false) => {
 	return data.access_token;
 };
 
-const addToPlaylist = async (accessToken, playlistId, uris, name) => {
+const addToPlaylist = async (accessToken, playlistId, uris, playlistUrl) => {
 
 	const response = await fetch(`https://api.spotify.com/v1/playlists/${encodeURIComponent(playlistId)}/tracks?uris=${uris.join(",")}`, {
 		method: "POST",
@@ -74,7 +88,7 @@ const addToPlaylist = async (accessToken, playlistId, uris, name) => {
 	const data = await response.json();
 
 	if (data.snapshot_id) {
-		console.log(`${name}: ${uris.length} succesfully added to playlist.`)
+		console.log(`${playlistUrl}: ${uris.length} succesfully added to playlist.`)
 	} else {
 		console.error("Error adding tracks:", data);
 	}
@@ -121,14 +135,13 @@ const findTrack = async (accessToken, name) => {
 	return uri;
 };
 
-const processRadio886Playlists = async (name, playlistId, accessToken) => {
-	const url = `https://meta.radio886.at/${name}`
-	const tracks = await getTracks(url);
+const processRadioPlaylists = async ({ url, spotifyPlaylistId, extractor }, accessToken) => {
+	const tracks = await getTracks(url, extractor);
 	const trackUris = await Promise.all(tracks.map(track => findTrack(accessToken, track)));
-	const existingTrackUris = await getPlaylistTracks(accessToken, playlistId);
+	const existingTrackUris = await getPlaylistTracks(accessToken, spotifyPlaylistId);
 	const newTrackUris = trackUris.filter(uri => !!uri && existingTrackUris.indexOf(uri) === -1);
 	if (newTrackUris.length > 0) {
-		await addToPlaylist(accessToken, playlistId, newTrackUris, name);
+		await addToPlaylist(accessToken, spotifyPlaylistId, newTrackUris, url);
 	}
 	return newTrackUris
 }
@@ -139,38 +152,36 @@ const processNewTracks = async () => {
 
 	const playlists = [
 		{
-			name: 'HardRock',
+			url: 'https://api.radiorock.sk/playlist',
+			spotifyPlaylistId: '1yhKr5UrTXJWRtCJSWZNWt',
+			extractor: extractors.mapRadioRock
+		},
+		{
+			url: 'https://meta.radio886.at/HardRock',
 			spotifyPlaylistId: '7Ec0rNNvVzASYJ6XaQmc4w',
+			extractor: extractors.map886,
 		},
 		{
-			name: 'NewRock',
+			url: 'https://meta.radio886.at/NewRock',
 			spotifyPlaylistId: '2VSTBfKYYUbZL3QORXYX7D',
+			extractor: extractors.map886,
 		},
 		{
-			name: 'ClassicRock',
+			url: 'https://meta.radio886.at/ClassicRock',
 			spotifyPlaylistId: '77N4roRdtoPPagHdBxjUwd',
+			extractor: extractors.map886,
 		},
 		{
-			name: '886',
+			url: 'https://meta.radio886.at/886',
 			spotifyPlaylistId: '1xv0WP1QQKYTeUN3LJ9UEy',
-		},
+			extractor: extractors.map886,
+		}
 	]
 
-	const spotifyPlaylistIdAllSongs = '2ADUOTr9QaI0sjBSXpyCTF'
-	let trackUris = []
-
 	for(let i = 0; i < playlists.length; i += 1) {
-		const { name, spotifyPlaylistId } = playlists[i]
-		const addedTrackUris = await processRadio886Playlists(name, spotifyPlaylistId, accessToken)
-		trackUris = [ ...trackUris, ...addedTrackUris ]
-		await sleep(1000 + Math.random() * 4000)
+		await processRadioPlaylists(playlists[i], accessToken)
 	}
 
-	const existingTrackUris = await getPlaylistTracks(accessToken, spotifyPlaylistIdAllSongs);
-	const newTrackUris = trackUris.filter(uri => !!uri && existingTrackUris.indexOf(uri) === -1);
-	if (newTrackUris.length > 0) {
-		await addToPlaylist(accessToken, spotifyPlaylistIdAllSongs, newTrackUris, 'All');
-	}
 };
 
 
@@ -184,8 +195,10 @@ const processNewTracks = async () => {
 // 7. uncomment console.logs in "auth()" below and run "auth(authCode, true)"
 // 8. put the value from "refresh_token" to env var "SPOTIFY_REFRESH_TOKEN"
 // register();
-// const authCode = "AQDbKFXQIGb3iaroiOMPL0WP1wrrNpLhhEkOuTu7FeGQLbM6X4Rhj98snMFjCIBr3_paZrBcm7I_ghU47ec76bPgWYteA2F5-XejzjUMpwg7BjPi2J5h2c-BRBeVNTSV7_SMQTNFirhx0iPNZTRIBgXTrQEYghElBABaJF4Rq4gEcoLFoKuB7Vf51dNVLmZoNncf3CbVqv_DchPGrWVVdbPgEocc9UB2PLqOfxDpwU5ffV9GmhChdTLRNBlWIWXFih5CbHc__Dt-VeEzsc7jwbOEHx9YCmbSwsvfB928XpIRxn5IxPmcYZvuZoac";
+// const authCode = "AQBNvn_g8Z3v52m8yVpecSAXuwEBBM1H84d7YXZagotvsmBb_6GzAKmu7ygrVKUJzMG6wOHXrxqhsdZR7T7dWfmwAd-MSWPNHQpbc3uCnv14i1ciF4pJ46x1wKg4iE-KwgGLteJk5xl3qH84_eqq4hyBqVTqnY5oRhR6AZ0_MNAwT9AdJMPhwoMWVoeVLmLoIRzC4Prfb4Iv-slqEOeuEf-wNUqXotVeOQEPHx3T-ovXj84Qbj_a7PpKKQnjgpx0ljIXTuyktzfRhDWMC4jCRaGIWZ2v6ioWzd6br9VIqEeX-_hXjxXvBKjrz0rH";
 // auth(authCode, true);
+
+
 
 // WORK WORK:
 processNewTracks();
