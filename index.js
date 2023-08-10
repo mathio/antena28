b7cf5f63643fe6aacd5a20713a248b820d40900b
Matej Lednicky <matej@lednicky.name>
2023-08-10 17:48:17 +0200

    add mongodb caching for spotify api calls

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 const fetch = require("isomorphic-unfetch");
 const btoa = require("btoa");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+const client = new MongoClient(process.env.MONGODB_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const getCachedTrack = async (key) => {
+  const one = await client.db("cache").collection("tracks").findOne({ key });
+  return one?.uri || null;
+};
+
+const setCachedTrack = async (key, uri) => {
+  await client.db("cache").collection("tracks").insertOne({ key, uri });
+};
+
+const getCachedPlaylist = async (key) => {
+  const one = await client.db("cache").collection("playlists").findOne({ key });
+  if (!one) {
+    return null;
+  }
+  const { uris, next } = one;
+  return { uris, next };
+};
+
+const setCachedPlaylist = async (key, uris, next) => {
+  await client
+    .db("cache")
+    .collection("playlists")
+    .insertOne({ key, uris, next });
+};
+
+const randomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const sleep = (msFrom, msTo) =>
+  new Promise((res) =>
+    setTimeout(res, msTo ? randomInt(msFrom, msTo) : msFrom),
+  );
 
 const extractors = {
   map886: (result) => {
@@ -24,11 +65,15 @@ const extractors = {
   },
 };
 
-const getTracks = async (url, extractor) => {
+const getTracks = async (url, extractor, asJson) => {
   let result = {};
   try {
     const response = await fetch(url);
-    result = await response.json();
+    if (asJson) {
+      result = await response.json();
+    } else {
+      result = await response.text();
+    }
   } catch (e) {
     console.error("Failed while fetching tracks:".e);
   }
@@ -81,20 +126,40 @@ const auth = async (code, useAuthCode = false) => {
   return data.access_token;
 };
 
-const addToPlaylist = async (accessToken, playlistId, uris, playlistUrl) => {
-  const response = await fetch(
-    `https://api.spotify.com/v1/playlists/${encodeURIComponent(
-      playlistId,
-    )}/tracks?uris=${uris.join(",")}`,
-    {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    },
-  );
+let spotifyGlobalAccessToken = null;
 
-  const data = await response.json();
+const fetchSpotify = async (method, path) => {
+  if (!spotifyGlobalAccessToken) {
+    const refreshToken = process.env.SPOTIFY_REFRESH_TOKEN;
+    spotifyGlobalAccessToken = await auth(refreshToken);
+  }
+
+  console.log("API:", path);
+
+  const response = await fetch(`https://api.spotify.com/v1${path}`, {
+    method,
+    headers: {
+      Authorization: `Bearer ${spotifyGlobalAccessToken}`,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(
+      `Failed while fetching ${path} (${response.status}): ${response.statusText}`,
+    );
+  }
+
+  await sleep(250, 1000);
+
+  return await response.json();
+};
+
+const addToPlaylist = async (playlistId, uris, playlistUrl) => {
+  const data = await fetchSpotify(
+    "POST",
+    `/playlists/${encodeURIComponent(playlistId)}/tracks?uris=${uris.join(
+      ",",
+    )}`,
+  );
 
   if (data.snapshot_id) {
     console.log(
@@ -105,81 +170,82 @@ const addToPlaylist = async (accessToken, playlistId, uris, playlistUrl) => {
   }
 };
 
-const getPlaylistTracks = async (accessToken, playlistId) => {
-  let playlistUrl = `https://api.spotify.com/v1/playlists/${encodeURIComponent(
-    playlistId,
-  )}/tracks`;
+const getPlaylistTracks = async (playlistId) => {
+  let playlistUrl = `/playlists/${encodeURIComponent(playlistId)}/tracks`;
   let trackUris = [];
 
   while (playlistUrl) {
-    const response = await fetch(playlistUrl, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    const data = await response.json();
-    const uris = (data.items && data.items.map((item) => item.track.uri)) || [];
-    trackUris = [...trackUris, ...uris];
-    playlistUrl = data.next;
-  }
+    const cached = await getCachedPlaylist(playlistUrl);
+    let uris = cached?.uris;
+    let nextUrl = cached?.next;
 
-  // console.log(trackUris);
+    if (!uris) {
+      const data = await fetchSpotify("GET", playlistUrl);
+      uris = (data.items && data.items.map((item) => item.track.uri)) || [];
+      nextUrl = data.next?.replace("https://api.spotify.com/v1", "") || null;
+    }
+
+    trackUris = [...trackUris, ...uris];
+
+    // there is next page = this page is already full = will not be modified = can be cached
+    if (nextUrl) {
+      await setCachedPlaylist(playlistUrl, uris, nextUrl);
+    }
+    playlistUrl = nextUrl;
+  }
 
   return trackUris;
 };
 
-const findTrack = async (accessToken, name) => {
-  const response = await fetch(
-    `https://api.spotify.com/v1/search?type=track&q=${encodeURIComponent(
-      name,
-    )}`,
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    },
-  );
-  const data = await response.json();
+const findTrack = async (name) => {
+  const query = encodeURIComponent(name);
 
-  const uri =
-    data.tracks && data.tracks.items.length > 0
-      ? data.tracks.items[0].uri
-      : null;
+  let uri = await getCachedTrack(query);
 
-  // console.log({name, uri});
+  if (!uri) {
+    const data = await fetchSpotify("GET", `/search?type=track&q=${query}`);
+    uri =
+      data.tracks && data.tracks.items.length > 0
+        ? data.tracks.items[0].uri
+        : null;
+  }
 
   if (!uri) {
     console.error("Unable to find track:", name);
   }
 
+  await setCachedTrack(query, uri);
+
   return uri;
 };
 
-const processRadioPlaylists = async (
-  { url, spotifyPlaylistId, extractor },
-  accessToken,
-) => {
-  const tracks = await getTracks(url, extractor);
-  const trackUris = await Promise.all(
-    tracks.map((track) => findTrack(accessToken, track)),
-  );
-  const existingTrackUris = await getPlaylistTracks(
-    accessToken,
-    spotifyPlaylistId,
-  );
+const findAllTracks = async (tracks) => {
+  const allTracks = [];
+  for (const track of tracks) {
+    allTracks.push(await findTrack(track));
+  }
+  return allTracks;
+};
+
+const processRadioPlaylists = async ({
+  url,
+  spotifyPlaylistId,
+  extractor,
+  asJson = true,
+}) => {
+  const tracks = await getTracks(url, extractor, asJson);
+  const trackUris = await findAllTracks(tracks);
+  const existingTrackUris = await getPlaylistTracks(spotifyPlaylistId);
   const newTrackUris = trackUris.filter(
     (uri) => !!uri && existingTrackUris.indexOf(uri) === -1,
   );
   if (newTrackUris.length > 0) {
-    await addToPlaylist(accessToken, spotifyPlaylistId, newTrackUris, url);
+    await addToPlaylist(spotifyPlaylistId, newTrackUris, url);
   }
   return newTrackUris;
 };
 
 const processNewTracks = async () => {
-  const refreshToken = process.env.SPOTIFY_REFRESH_TOKEN;
-  const accessToken = await auth(refreshToken);
-
   const playlists = [
     {
       url: "https://api.radiorock.sk/playlist",
@@ -209,8 +275,13 @@ const processNewTracks = async () => {
   ];
 
   for (let i = 0; i < playlists.length; i += 1) {
-    await processRadioPlaylists(playlists[i], accessToken);
+    if (i > 0) {
+      await sleep(1000, 3000);
+    }
+    await processRadioPlaylists(playlists[i]);
   }
+
+  process.exit(0);
 };
 
 // SETUP:
@@ -223,7 +294,7 @@ const processNewTracks = async () => {
 // 7. uncomment console.logs in "auth()" below and run "auth(authCode, true)"
 // 8. put the value from "refresh_token" to env var "SPOTIFY_REFRESH_TOKEN"
 // register();
-// const authCode = "AQBNvn_g8Z3v52m8yVpecSAXuwEBBM1H84d7YXZagotvsmBb_6GzAKmu7ygrVKUJzMG6wOHXrxqhsdZR7T7dWfmwAd-MSWPNHQpbc3uCnv14i1ciF4pJ46x1wKg4iE-KwgGLteJk5xl3qH84_eqq4hyBqVTqnY5oRhR6AZ0_MNAwT9AdJMPhwoMWVoeVLmLoIRzC4Prfb4Iv-slqEOeuEf-wNUqXotVeOQEPHx3T-ovXj84Qbj_a7PpKKQnjgpx0ljIXTuyktzfRhDWMC4jCRaGIWZ2v6ioWzd6br9VIqEeX-_hXjxXvBKjrz0rH";
+// const authCode = "AQCAl_83rjvWj7psdI1cfw967NdT-088r_If9ZsVEhJn1M9i3NVQJ9NdAjFkvmjad0whvSnBs55eJDGaATEIF4bOJ4LIRmJUY1enl5GbWwLahM85S3Rwvgv577LQhejqt5fLdPPZEH76Jj-CIqDB9xgZOmMrAQgWa16nc_UQhJegxpMqUi1GGxUrh9NsxW9omEaqPRohh3nULBdJJ49K18vNlu6G30q5La67jhesu4k38oIiXPFBgFZnixA-RU1ABPDIrUfhyJu7V8XaiuMkoOrstpEuCNeqvXK79A8CQUBd_7zpamkfCTs6GD1O";
 // auth(authCode, true);
 
 // WORK WORK:
